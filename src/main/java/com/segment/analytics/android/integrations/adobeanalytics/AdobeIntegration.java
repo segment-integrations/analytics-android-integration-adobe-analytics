caa639a1fbfce64454248af409cac266881b0e3e
Brennan Gamwell <brennan@segment.com>
2017-11-14 16:40:00 -0800

    Refactor track, trackEcommerce and screen methods

--- src/main/java/com/segment/analytics/android/integrations/adobeanalytics/AdobeIntegration.java
+++ src/main/java/com/segment/analytics/android/integrations/adobeanalytics/AdobeIntegration.java
@@ -8,6 +8,7 @@ import android.support.annotation.Nullable;
 import android.text.TextUtils;
 import com.adobe.mobile.Config;
 import com.segment.analytics.Analytics;
+import com.segment.analytics.Properties.Product;
 import com.segment.analytics.Properties;
 import com.segment.analytics.Traits;
 import com.segment.analytics.ValueMap;
@@ -65,16 +66,24 @@ public class AdobeIntegration extends Integration<com.adobe.mobile.Analytics> {
   private static final String ADOBE_KEY = "Adobe Analytics";
   private final Logger logger;
   private final Map<String, Object> events;
-  private final Map<String, Object> eVars;
+  private final Map<String, Object> contextDataMapper;
   private final Map<String, Object> lVars;
-  private final Map<String, Object> props;
+  private static final Map<String, String> ECOMMERCE_MAPPER = createEcommerceMap();
+  private static Map<String, String> createEcommerceMap() {
+    Map<String, String> map = new HashMap<>();
+    map.put("Product Added", "scAdd");
+    map.put("Product Removed", "scRemove");
+    map.put("Cart Viewed", "scView");
+    map.put("Checkout Started", "scCheckout");
+    map.put("Order Completed", "purchase");
+    return map;
+  }
 
   AdobeIntegration(ValueMap settings, Logger logger) {
     this.logger = logger;
     this.events = settings.getValueMap("events");
-    this.eVars = settings.getValueMap("eVars");
+    this.contextDataMapper = settings.getValueMap("cDataMapper");
     this.lVars = settings.getValueMap("lVars");
-    this.props = settings.getValueMap("props");
   }
 
   @Override
@@ -131,14 +140,18 @@ public class AdobeIntegration extends Integration<com.adobe.mobile.Analytics> {
 
     String eventName = track.event();
 
+    if (ECOMMERCE_MAPPER.containsKey(eventName)) {
+      trackEcommerce(track);
+    }
+
     if (isNullOrEmpty(events) || !events.containsKey(eventName)) {
       logger.verbose("Please map your event names to corresponding "
           + "Adobe event names in your Segment UI.");
       return;
     }
 
-    eventName = String.valueOf(events.get(eventName));
     Properties properties = track.properties();
+    eventName = String.valueOf(events.get(eventName));
 
     if (isNullOrEmpty(properties)) {
       com.adobe.mobile.Analytics.trackAction(eventName, null);
@@ -152,30 +165,38 @@ public class AdobeIntegration extends Integration<com.adobe.mobile.Analytics> {
     logger.verbose("Analytics.trackAction(%s, %s);", eventName, mappedProperties);
   }
 
+  private void trackEcommerce(TrackPayload track) {
+    String eventName = ECOMMERCE_MAPPER.get(track.event());
+    Properties properties = track.properties();
+    Properties propertiesCopy = null;
+    Map<String, Object> ecommerceProperties = null;
+
+    if (!isNullOrEmpty(properties)) {
+      propertiesCopy = new Properties();
+      propertiesCopy.putAll(properties);
+      propertiesCopy.remove("products");
+      ecommerceProperties = mapEcommerce(eventName, properties);
+    }
+    // pass in the copy of properties without a nested products array
+    Properties mappedProperties = mapProperties(propertiesCopy);
+    mappedProperties.putAll(ecommerceProperties);
+
+    com.adobe.mobile.Analytics.trackAction(eventName, mappedProperties);
+    logger.verbose("Analytics.trackAction(%s, %s);", eventName, mappedProperties);
+  }
+
   private Properties mapProperties(Properties properties) {
     Properties propertiesCopy = new Properties();
     propertiesCopy.putAll(properties);
     Properties mappedProperties = new Properties();
 
-    if (!isNullOrEmpty(eVars)) {
+    if (!isNullOrEmpty(contextDataMapper)) {
       for (Map.Entry<String, Object> entry : properties.entrySet()) {
         String property = entry.getKey();
         Object value = entry.getValue();
 
-        if (eVars.containsKey(property)) {
-          mappedProperties.put(String.valueOf(eVars.get(property)), value);
-          propertiesCopy.remove(property);
-        }
-      }
-    }
-
-    if (!isNullOrEmpty(props)) {
-      for (Map.Entry<String, Object> entry : properties.entrySet()) {
-        String property = entry.getKey();
-        Object value = entry.getValue();
-
-        if (props.containsKey(property)) {
-          mappedProperties.put(String.valueOf(props.get(property)), value);
+        if (contextDataMapper.containsKey(property)) {
+          mappedProperties.put(String.valueOf(contextDataMapper.get(property)), value);
           propertiesCopy.remove(property);
         }
       }
@@ -203,6 +224,48 @@ public class AdobeIntegration extends Integration<com.adobe.mobile.Analytics> {
     // pass along remaining unmapped Segment properties as contextData values
     mappedProperties.putAll(propertiesCopy);
     return mappedProperties;
+  }
+
+  private Map<String, Object> mapEcommerce (String eventName, Properties properties) {
+    StringBuilder productStringBuilder = new StringBuilder();
+    Map<String, Object> contextData = new HashMap<>();
+    String concatenatedValues;
+
+    List<Product> products = properties.products();
+    if (!isNullOrEmpty(products)) {
+      for (int i = 0; i < products.size(); i++) {
+        Product product = products.get(i);
+        // adobe requires we pass a semicolon as a placeholder even if a value is empty
+        String category = ";";
+        String name = ";";
+        String quantity = "1;";
+        String price = ";";
+        if (product.containsKey("category")) {
+          category = product.getString("category") + ";";
+        }
+        if (product.containsKey("name")) {
+          name = product.getString("name") + ";";
+        }
+        if (product.containsKey("quantity")) {
+          quantity = product.getString("quantity") + ";";
+        }
+        if (product.containsKey("price")) {
+          price = product.getString("price") + ";";
+        }
+        if (i < products.size() - 1) {
+          concatenatedValues = category + name + quantity + price + ",";
+        } else {
+          concatenatedValues = category + name + quantity + price;
+        }
+        productStringBuilder.append(concatenatedValues);
+      }
+      String productString = productStringBuilder.toString();
+      if (eventName.equals("purchase")) {
+        contextData.put("purchaseid", properties.getString("orderId"));
+      }
+      contextData.put("&&products", productString);
+    }
+    return contextData;
   }
 
   @Override
